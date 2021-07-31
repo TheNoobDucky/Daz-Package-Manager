df28dd36b2c46bc71f7ee252583c503f78f4c13e
Quack <thenoobducky@gmail.com>
2021-07-31 18:29:06 +1000

    enable warn missing file by default

--- Daz Package Manager/Properties/Settings.Designer.cs
+++ Daz Package Manager/Properties/Settings.Designer.cs
@@ -121,7 +121,7 @@ namespace Daz_Package_Manager.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool WarnMissingFile {
             get {
                 return ((bool)(this["WarnMissingFile"]));
